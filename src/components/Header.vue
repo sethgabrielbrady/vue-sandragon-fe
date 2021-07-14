3b47a1e4f328e29255a2635c8e6ab86cec1f7528
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-07-14 14:50:08 -0400

    Adds signup

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="bg-grey-lighter py-4">
+    <div class="container m-auto flex flex-wrap items-center justify-end">
+      <div>
+        <router-link to="/signin" class="link-grey px-2 no-underline" v-if="!signedIn()">Sign in</router-link>
+        <router-link to="/signup" class="link-grey px-2 no-underline" v-if="!signedIn()">Sign Up</router-link>
+        <router-link to="/elements" class="link-grey px-2 no-underline" v-if="signedIn()">Elements</router-link>
+        <a href="#" @click.prevent="signOut" class="link-grey px-2 no-underline" v-if="signedIn()">Sign out</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  created () {
+    this.signedIn()
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
+    signedIn () {
+      return localStorage.signedIn
+    },
+    // signOut () {
+    //   this.$http.secured.delete('/signin')
+    //     .then((response) => {
+    //       delete localStorage.csrf
+    //       delete localStorage.signedIn
+    //       this.$router.replace('/')
+    //     })
+    //     .catch(error => this.setError(error, 'Cannot sign out'))
+    // }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.js';
 import App from './App.vue'
+import router from './router.js';
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
+import Router from 'vue-router'
+
+
+Vue.use(Router);
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  securedAxiosInstance,
+  plainAxiosInstance,
+  components: { App },
+  template: '<App/>'
+})
