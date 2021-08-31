433698b49f2274d18fc8666f2546c1d4622c83f6
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-08-30 21:27:01 -0400

    Updates posts edit

--- src/components/Materials/editor.vue
+++ src/components/Materials/editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="items-center text-center p-4">
-    <h1>Posts edit Page</h1>
+    <h1>Materials edit Page</h1>
     <h2>This will be an admin only view</h2>
   </div>
 </template>
--- src/components/Posts/edit.vue
+++ src/components/Posts/edit.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="p-4 text-center">
-    <h1>Post Edit View</h1>
-    <div class="mx-auto border w-1/5 text-center p-4">
-      <form>
+  <div class="p-4 ">
+    <h1 class="text-center p-4">Post Edit View</h1>
+    <div class="w-full max-w-5xl mx-auto">
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2 p-2" for="title">Title</label>
+        <input class="max-w-xs shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="post-title" type="text" placeholder="Title">
+
+        <label class="block text-gray-700 text-sm font-bold mb-2 p-2" for="body">Body</label>
+        <input class="max-w-2xl resize resize-y shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40" id="post-body" type="textarea" placeholder="Body">
+
+        <label class="block text-gray-700 text-sm font-bold mb-2 p-2" for="username">Author</label>
+        <input class="max-w-xs shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="post-author" type="text" placeholder="Author">
+
+        <label class="block text-gray-700 text-sm font-bold mb-2 p-2" for="image">Post Image</label>
         <input class="mb-4" type="file" name="image" ref="inputFile" @change=uploadFile()>
+        <br>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click=createItem>Upload</button>
       </form>
     </div>
@@ -11,41 +22,43 @@
 </template>
 
 <script>
-//Here I need to store post data into state, maybe?
 export default {
   name: 'Edit',
   data () {
     return {
-      inputPicture: null
+      inputPicture: null,
+      poatId: 1,
+      title: "Title Test 13",
+      body: "Description test",
+      author: "Test Author"
+
     }
   },
   methods: {
-    // Saving the file in our data to send it !
     uploadFile: function() {
       this.inputPicture = this.$refs.inputFile.files[0];
     },
-
-    // Collecting everything inside our FormData object
-    createItem: function() {
-      console.log(this.inputPicture)
+    createItem: function(event) {
       const params = {
         'image': this.inputPicture
       }
 
       let formData = new FormData()
-      // formData.append('image', this.inputPicture)
       Object.entries(params).forEach(
         ([key, value]) => formData.append(key, value)
       )
 
-      formData.forEach((value, key) => {
-        console.log("key %s: value %s", key, value.name);
-      })
+      event.preventDefault
+      if(this.inputPicture){
+        this.$http.uploadFile.patch(`/materials/${this.materialId}`, formData)
+      }
 
-      // Finally, sending the POST request with our beloved Axios
-      this.$http.upload.put('/posts/1',
-        formData
-      )
+      this.$http.plain.patch(`/post/${this.materialId}`, {
+        material: {
+          title: this.title,
+          description: this.description,
+        }
+      })
     }
   }
 }
