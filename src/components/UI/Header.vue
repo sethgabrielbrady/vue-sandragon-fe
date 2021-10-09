468504086b4e4be35c6b5a3836769633e1569277
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-10-09 17:27:37 -0400

    Adds about pages

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -9,6 +9,11 @@
           <p clas="inline-block">SANDRAGON</p>
         </router-link>
         <div>
+          <router-link to="/about" class="text-white px-2 no-underline inline-block">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sd-orange" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+          </router-link>
           <p class="text-white px-2 no-underline inline block" v-if="!signedIn()" @click="checkUrl">Sign in</p>
           <router-link to="/materials" class="text-white px-2 no-underline inline-block" v-if="signedIn() && showAdminLink()">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sd-orange" fill="none" viewBox="0 0 24 24" stroke="currentColor">
